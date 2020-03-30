a3eaecd4b8c072dea0283c5c461945dba33161d7
TheStrangeProgrammer <48047563+TheStrangeProgrammer@users.noreply.github.com>
2020-03-30 04:37:42 +0300

    Smooth camera done

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -4,24 +4,43 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-
+    Transform pivot;
     public static CameraController cameraController;
+    public Camera mainCamera;
 
     public float panSpeed = 5;
     private float zoomedInAngle = 0;
     private float zoomedOutAngle = 0;
-    private float minZoom = 0; 
-    private float maxZoom = 0; 
+    private float minZoom = 5; 
+    private float maxZoom = 200; 
     public bool inverseZoom = false;
     private Vector3 MouseStart, MouseMove;
     private Vector3 derp;
     public static Quaternion currentAngle;
 
     public float zoomLevel { get; set; }
+    float targetPanningX;
+    float targetPanningY;
+    float targetZoom;
+    float targetZoomAngle;
+    float targetRotation;
 
+    float currentPanningVelocityX;
+    float currentPanningVelocityY;
+    float currentZoomVelocity;
+    float currentZoomAngleVelocity;
+    float currentRotationVelocity;
+
+    float smoothPanningTimeX = 0.3f;
+    float smoothPanningTimeY = 0.3f;
+    float smoothZoomTime=0.3f;
+    float smoothZoomAngleTime = 0.3f;
+    float smoothRotationTime = 0.3f;
     // Used before Start()
     void Awake()
     {
+        pivot = new GameObject().transform;
+        pivot.position = gameObject.transform.position;
         zoomLevel = 0;
         cameraController = this;
         ResetCamera();
@@ -34,12 +53,12 @@ public class CameraController : MonoBehaviour
         maxZoom = 200;
     }
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
 
         ChangeZoom();
         ChangePosition();
-
+        ChangeRotation();
     }
 
     // This method pans the camera view in the XZ plane
@@ -47,14 +66,18 @@ public class CameraController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            MouseStart = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+            MouseStart = mainCamera.ViewportToWorldPoint(Input.mousePosition);
         }
-        else if (Input.GetMouseButton(0))
+           else if (Input.GetMouseButton(0))
         {
-            MouseMove = new Vector3(Input.mousePosition.x - MouseStart.x, Input.mousePosition.y - MouseStart.y);
-            MouseStart = new Vector3(Input.mousePosition.x, Input.mousePosition.y, gameObject.transform.position.z);
-            transform.position = new Vector3(transform.position.x - MouseMove.x * Time.deltaTime*panSpeed, transform.position.y - MouseMove.y * Time.deltaTime * panSpeed, gameObject.transform.position.z);
+            MouseMove = mainCamera.ViewportToWorldPoint(Input.mousePosition);
+            targetPanningX = transform.position.x - (Input.GetAxis("Mouse X")* transform.up.y + Input.GetAxis("Mouse Y") * transform.up.x)*panSpeed;
+            targetPanningY = transform.position.y - (Input.GetAxis("Mouse Y") * transform.right.x+ Input.GetAxis("Mouse X") * transform.right.y) * panSpeed;
+            MouseStart = mainCamera.ViewportToWorldPoint(Input.mousePosition);
         }
+        transform.position = new Vector3(Mathf.SmoothDamp(transform.position.x, targetPanningX,ref currentPanningVelocityX,smoothPanningTimeX),
+            Mathf.SmoothDamp(transform.position.y, targetPanningY, ref currentPanningVelocityY, smoothPanningTimeY),
+            gameObject.transform.position.z);
     }
 
     // This method changes the zoom of the camera
@@ -71,15 +94,35 @@ public class CameraController : MonoBehaviour
             {
                 zoomLevel = Mathf.Clamp01(zoomLevel + Input.GetAxis("Mouse ScrollWheel"));
             }
-
-            float zoom = Mathf.Lerp(-minZoom, -maxZoom, zoomLevel);
-            gameObject.transform.localPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, zoom);
-
-            float zoomAngle = Mathf.Lerp(zoomedInAngle, zoomedOutAngle, zoomLevel);
-            gameObject.transform.localRotation = Quaternion.Euler(zoomAngle, 0, 0);
-            currentAngle = gameObject.transform.localRotation;
-
+            targetZoom = Mathf.Lerp(-minZoom, -maxZoom, zoomLevel);
+            targetZoomAngle = Mathf.Lerp(zoomedInAngle, zoomedOutAngle, zoomLevel);
         }
+            
+            mainCamera.transform.localPosition = new Vector3(mainCamera.transform.localPosition.x, mainCamera.transform.localPosition.y, Mathf.SmoothDamp(mainCamera.transform.localPosition.z,targetZoom,ref currentZoomVelocity,smoothZoomTime));
+
+
+        mainCamera.transform.localRotation = Quaternion.Euler(
+                Mathf.SmoothDampAngle(mainCamera.transform.localRotation.eulerAngles.x,targetZoomAngle, ref currentZoomAngleVelocity, smoothZoomAngleTime),
+                mainCamera.transform.localRotation.eulerAngles.y,
+                mainCamera.transform.localRotation.eulerAngles.z
+                );
+            currentAngle = mainCamera.transform.localRotation;
+
+        
+    }
+    void ChangeRotation() {
+
+        if (Input.GetMouseButton(1))
+        {
+            targetRotation = transform.eulerAngles.z  + Input.GetAxis("Mouse X") * panSpeed;
+            //transform.RotateAround(transform.position, Vector3.up, pos.x);
+            //Debug.Log(gameObject.transform.eulerAngles.z.ToString()+targetZoomAngle.ToString());
+        }
+            gameObject.transform.localRotation = Quaternion.Euler(
+            gameObject.transform.eulerAngles.x,
+            gameObject.transform.eulerAngles.y,
+            Mathf.SmoothDampAngle(gameObject.transform.eulerAngles.z,  targetRotation, ref currentRotationVelocity, smoothRotationTime));
+        currentAngle = gameObject.transform.localRotation;
     }
 
 
@@ -88,9 +131,9 @@ public class CameraController : MonoBehaviour
     {
         gameObject.transform.position = new Vector3(0, 0, 0);
         zoomLevel = 0;
-        gameObject.transform.rotation = Quaternion.Euler(zoomedInAngle, 0, 0);
+        gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
         currentAngle = gameObject.transform.rotation;
-        gameObject.transform.localPosition = new Vector3(0, 0, -minZoom);
+        gameObject.transform.localPosition = new Vector3(0, 0, 0);
     }
 
     // This method stops the camera 
